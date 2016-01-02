8ba07b19d27a2f32f1da7619f3794e8b268dc2a6
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-02 13:04:02 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,185 @@
+var onfleet = require('./onfleet.js')
+var connection = require('./database.js')
+
+var path = require('path')
+var validator = require('validator')
+var bcrypt = require('bcrypt')
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+// app.use(express.static('public'));
+
+// Main GET request listener
+app.get('/PiggyBack', function(request, response) {
+	onfleet.getSingleTeamByName('TEST').then(function(team) {
+		onfleet.getOrganizationDetails().then(function(org) {
+			onfleet.getDestinationByID('IWU6PFSVyLhAbifvh3KnDxnZ').then(function(destination) {
+				onfleet.listTasks().then(function(tasks) {
+					onfleet.listWebHooks().then(function(webhooks) {
+						response.render(
+							'index', {
+								pageTitle: 'NOAH', 
+								orgName: org.name, 
+								orgID: org.id,
+								orgEmail: org.email,
+								teamName: team.name,
+								teamID: team.id, 
+								teamWorkers: team.workers,
+								destination: destination,
+								tasks: tasks,
+								webhooks: webhooks
+							}
+						)
+					})
+				})
+			})
+		})
+	})
+})
+
+
+// POST request listener for a call to create a new worker
+app.post('/PiggyBack/new-worker', function(request, response) {
+	if (!request.body.name || !request.body.number)
+		response.redirect('/')
+	else {
+		onfleet.createNewWorker(
+			request.body.name,
+			request.body.number.replace(/[^0-9\+]/, ''),
+			['ylC5klVbtmEVrVlBfUYp9oeM'], 
+			{
+				type: 'CAR', 
+				description: 'Lamborghini', 
+				licensePlate: '333-ABC', 
+				color: 'black'
+			}
+		).then(function(worker) {
+			response.redirect('/PiggyBack/')
+		}).catch(function(error) {
+			response.render('error', {pageTitle: 'Error', error: JSON.stringify(error)})
+		})
+	}
+})
+
+// POST request listener for a call to delete a worker
+	// need to pass in worker's id
+app.post('/PiggyBack/delete-worker', function(request, response) {
+	if (!request.body.id)
+		response.redirect('/PiggyBack/')
+	else {
+		onfleet.deleteWorkerByID(request.body.id).then(function() {
+			response.redirect('/PiggyBack/')
+		}).catch(function(error) {
+			response.render('error', {pageTitle: 'Error', error: JSON.stringify(error)})
+		})
+	}
+})
+
+// POST request listener for a call to update a worker
+	// need to pass in worker's id and probably more
+app.post('/PiggyBack/update-worker', function(request, response) {
+	if (!(request.body.id && request.body.name))
+		response.redirect('/PiggyBack/')
+	else {
+		onfleet.updateWorkerByID(request.body.id, {name: request.body.name}).then(function() {
+			response.redirect('/PiggyBack/')
+		}).catch(function(error) {
+			response.render('error', {pageTitle: 'Error', error: JSON.stringify(error)})
+		})
+	}
+})
+
+app.post('/PiggyBack/new-destination', function(request, response) {
+	if (!(request.body.number && request.body.street && request.body.city && request.body.country))
+		response.redirect('/PiggyBack/')
+	else {
+		onfleet.createNewDestination(
+			{
+				name: request.body.name,
+				number: request.body.number,
+				street: request.body.street,
+				apartment: request.body.apartment,
+				city: request.body.city,
+				state: request.body.state,
+				postalCode: request.body.postalCode,
+				country: request.body.country
+			}
+		).then(function(destination) {
+			console.log(JSON.stringify(destination))
+			response.redirect('/PiggyBack/')
+		}).catch(function(error) {
+			response.render('error', {pageTitle: 'Error', error: JSON.stringify(error)})
+		})
+	}
+})
+
+app.get('/PiggyBack/signup', function(request, response) {
+	response.render('signup', {pageTitle: 'Sign up'})
+})
+
+app.post('/PiggyBack/signup', function(request, response) {
+	var username = request.body.username
+	var firstname = request.body.firstname
+	var lastname = request.body.lastname
+	var phone = request.body.phone
+	var password = request.body.password
+	var password2 = request.body.password2
+
+	if (!(username && firstname && lastname && phone && password && password2)) {
+		response.render('signup', {pageTitle: 'Sign up', errors: ['All fields must be completed'], username: username, firstname: firstname, lastname: lastname, phone: phone})
+		return
+	}
+	errors = []
+	if (!validator.isAlphanumeric(username))
+		errors.push('Username must contain only letters and numbers')
+	if (!validator.isAlpha(firstname))
+		errors.push('First Name must contain only letters')
+	if (!validator.isAlpha(lastname))
+		errors.push('Last Name must contain only letters')
+	if (!(phone.length == 12 && phone.charAt(3) == '-' && phone.charAt(7) == '-' && validator.isInt(phone.substring(0, 3) + phone.substring(4, 7) + phone.substring(8))))
+		errors.push('Phone Number must be of the form 123-456-7890')
+	if (password.length < 6)
+		errors.push('Password length must be at least 6')
+	if (!validator.isAscii(password))
+		errors.push('Password contains invalid characters')
+	if (password.localeCompare(password2) != 0)
+		errors.push('Passwords must match')
+	if (errors.length) {
+		response.render('signup', {pageTitle: 'Sign up', errors: errors, username: username, firstname: firstname, lastname: lastname, phone: phone})
+		return
+	}
+
+	//response.render('success', {pageTitle: 'Success'})
+	connection.query('SELECT id FROM users WHERE username=?', [username], function(error, rows) {
+		if (error) 
+			throw error
+		if (rows.length) {
+			response.render('signup', {pageTitle: 'Sign up', errors: ['Username already taken'], username: username, firstname: firstname, lastname: lastname, phone: phone})
+			return
+		}
+		bcrypt.hash(password, 8, function(error, hash) {
+			if (error)
+				throw error
+			connection.query('INSERT INTO Users (username, firstname, lastname, password, phone) VALUES (?,?,?,?,?)',
+				[username, firstname, lastname, hash, phone], function(error, rows) {
+					if (error)
+						throw error
+					response.render('success', {pageTitle: 'Success'})
+				})
+		})
+	})
+})
+
+http.listen(8080, '127.0.0.1', function() {
+	console.log('listening on port 8080')
+})
---
+++ onfleet.js
@@ -0,0 +1,250 @@
+var Promise = require('promise')
+var https = require('https')
+
+var apiKey = '2a1a17056d3c4677ee5fe70a0c213f00'
+var hostname = 'onfleet.com'
+var path = '/api/v2/'
+
+function request(endpoint, method, data) {
+	return new Promise(function(resolve, reject) {
+		var request = https.request({
+			hostname: hostname,
+			path: path + endpoint,
+			method: method,
+			auth: apiKey
+		}, function(response) {
+			var str = ''
+			response.on('data', function(chunk) {
+				str += chunk
+			})
+			response.on('end', function() {
+				var data
+				if (str.length)
+					data = JSON.parse(str)
+				
+				if (response.statusCode != 200) {
+					reject(data)
+				}
+				else {
+					resolve(data)
+				}
+			})
+		})
+		request.on('error', function(error) {
+			reject(error)
+		})
+		if (data)
+			request.write(JSON.stringify(data))
+		request.end()
+	})
+}
+
+// ORGANIZATIONS
+// A delegatee is an organization to which the current organization is able to assign tasks.
+function getOrganizationDetails() {
+	return request('organization', 'GET')
+}
+function getDelegateeDetailsByID(id) {
+	return request('organizations/' + id, 'GET')
+}
+function getDelegateeDetailsByName(name) {
+	return new Promise(function(resolve, reject) {
+		getDetails().then(function(organization) {
+			for (var i = 0; i < organization.delegatees.length; ++i) {
+				request('organizations/' + organization.delegatees[i], 'GET').then(function(delegatee) {
+					if (delegatee.name.localeCompare(name) == 0) {
+						getDelegateeDetailsByID(delegatee.id).then(function(delegatee) {
+							resolve(delegatee)
+						}).catch(function(error) {
+							reject(error)
+						})
+					}
+				})
+			}
+		}).catch(function(error) {
+			reject(error)
+		})
+	})
+}
+
+// ADMINISTRATORS
+// When a new administrator is created, an email will be sent to confirm and configure their account
+function createNewAdministrator(name, email, phone, vehicle) {
+	return request('workers', 'POST', {'name': name, 'email': email, 'phone': phone})
+}
+function listAdministrators() {
+	return request('admins', 'GET')
+}
+function updateAdministratorByID(id, data) {
+	return request('admins/' + id, 'PUT', data)
+}
+function deleteAdministratorByID(id) {
+	return request('admins/' + id, 'DELETE')
+}
+
+// WORKERS
+function createNewWorker(name, phone, teams, vehicle) {
+	return request('workers', 'POST', {name: name, phone: phone, teams: teams, vehicle: vehicle})
+}
+function listWorkers() {
+	return request('workers', 'GET')
+}
+function getSingleWorkerByID(id) {
+	return request('workers/' + id, 'GET')
+}
+function updateWorkerByID(id, data) {
+	return request('workers/' + id, 'PUT', data)
+}
+function deleteWorkerByID(id) {
+	return request('workers/' + id, 'DELETE')
+}
+
+// TEAMS
+// Teams must be created, updated and deleted via the dashboard.
+function listTeams() {
+	return request('teams', 'GET')
+}
+function getSingleTeamByID(id) {
+	return request('teams/' + id, 'GET')
+}
+function getSingleTeamByName(name) {
+	return new Promise(function(resolve, reject) {
+		listTeams().then(function(teams) {
+			for (var i = 0; i < teams.length; ++i) {
+				if ((teams[i].name).localeCompare(name) == 0)
+					getSingleTeamByID(teams[i].id).then(function(team) {
+						resolve(team)
+					}).catch(function(error) {
+						reject(error)
+					})
+			}
+		}).catch(function(error) {
+			reject(error)
+		})
+	})
+}
+
+// DESTINATIONS
+	// address = {name, *number, *street, apartment, *city, state, postalCode, *country, unparsed}
+function createNewDestination(address) {
+	return request('destinations', 'POST', {address: address})
+}
+function getDestinationByID(id) {
+	return request('destinations/' + id, 'GET')
+}
+
+// RECIPIENTS
+	// cannot be deleted once created
+function createNewRecipient(name, phone, notes, skipSMSNotifications, skipPhoneNumberValidation) {
+	return request(
+		'recipients', 
+		'POST', 
+		{
+			name: name, 
+			phone: phone, 
+			notes: notes,
+			skipSMSNotifications: skipSMSNotifications,
+			skipPhoneNumberValidation: skipPhoneNumberValidation
+		}
+	)
+}
+function updateRecipientByID(id, data) {
+	return request('recipients/' + id, 'PUT', data)
+}
+function findRecipientByName(name) {
+	return request('recipients/name/' + name, 'GET')
+}
+function findRecipientByPhone(phone) {
+	return request('recipients/phone/' + phone, 'GET')
+}
+function getSingleRecipient(id) {
+	return request('recipients/' + id, 'GET')
+}
+
+// TASKS
+	// state = (0: unassigned, 1: assigned, 2: active, 3: completed)
+function createNewTask(merchant, executor, destination, recipients, completeAfter,
+	completeBefore, pickupTask, dependendencies, notes, autoAssign) {
+	return request(
+		'tasks', 
+		'POST', 
+		{
+			merchant: merchant,
+			executor: executor,
+			destination: destination,
+			recipients: recipients,
+			completeAfter: completeAfter,
+			completeBefore: completeBefore,
+			pickupTask: pickupTask,
+			dependendencies: dependendencies,
+			notes: notes,
+			autoAssign: autoAssign
+		}
+	)
+}
+function listTasks() {
+	return request('tasks', 'GET')
+}
+function getSingleTask(id) {
+	return request('tasks/' + id, 'GET')
+}
+function updateTask(id, data) {
+	return request('tasks/' + id, 'PUT', data)
+}
+function completeTask(id, success) {
+	return request('tasks/' + id + '/complete', 'POST', {success: success})
+}
+function deleteTask(id) {
+	return request('tasks/' + id, 'DELETE')
+}
+
+// WEBHOOKS
+function createWebHook(url, trigger) {
+	return request('webhooks', 'POST', {url: url, trigger: trigger})
+}
+function listWebHooks() {
+	return request('webhooks', 'GET')
+}
+function deleteWebHook(id) {
+	return request('webhooks/' + id, 'DELETE')
+}
+module.exports = {
+	getOrganizationDetails: getOrganizationDetails, 
+	getDelegateeDetailsByID: getDelegateeDetailsByID,
+	getDelegateeDetailsByName: getDelegateeDetailsByName,
+
+	createNewAdministrator: createNewAdministrator,
+	listAdministrators: listAdministrators,
+	updateAdministratorByID: updateAdministratorByID,
+	deleteAdministratorByID: deleteAdministratorByID,
+
+	createNewWorker: createNewWorker,
+	listWorkers: listWorkers,
+	getSingleWorkerByID: getSingleWorkerByID,
+	updateWorkerByID: updateWorkerByID,
+	deleteWorkerByID: deleteWorkerByID,
+
+	createNewDestination: createNewDestination,
+	getDestinationByID: getDestinationByID,
+
+	listTeams: listTeams, 
+	getSingleTeamByID: getSingleTeamByID,
+	getSingleTeamByName: getSingleTeamByName,
+
+	createNewRecipient: createNewRecipient,
+	updateRecipientByID: updateRecipientByID,
+	findRecipientByName: findRecipientByName,
+	findRecipientByPhone: findRecipientByPhone,
+	getSingleRecipient: getSingleRecipient,
+
+	createNewTask: createNewTask,
+	listTasks: listTasks,
+	getSingleTask: getSingleTask,
+	updateTask: updateTask,
+	completeTask: completeTask,
+	deleteTask: deleteTask,
+
+	createWebHook: createWebHook,
+	listWebHooks: listWebHooks,
+	deleteWebHook: deleteWebHook
+}
