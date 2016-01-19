1ae1ab2ffa48f7063b409478d77e98173b2bb5c3
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-19 14:17:25 -0800

    Deploy

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ app.get('/', function(request, response) {
 
 		if (request.session.views) {
 			request.session.views++
-			response.render('index', {pageTitle: 'Home', views: request.session.views, username: username, password: JSON.stringify(response)})
+			response.render('index', {pageTitle: 'Home', views: request.session.views, username: username, password: password})
 		} else {
 			request.session.views = 1
-			response.render('index', {pageTitle: 'Home', views: request.session.views, username: username, password: JSON.stringify(response)})
+			response.render('index', {pageTitle: 'Home', views: request.session.views, username: username, password: password})
 		}
 	})
 })
