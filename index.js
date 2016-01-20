afc332c8149234840be09cbb97678f4ddba69f01
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-20 14:53:48 -0800

    Deploy

--- index.js
+++ index.js
@@ -78,8 +78,7 @@ app.post('/', function(request, response) {
 			password=parts[1];
 
 		response.writeHead(200, { 'Content-Type': 'text/plain' });
-		console.log(JSON.stringify(request.body))
-		response.write(JSON.stringify(request.body))
+		response.write(JSON.stringify(request.body.name))
 		// response.write("{header:Test Page, username:" + username + ", password:" + password + "}");
 		response.end();
 })
