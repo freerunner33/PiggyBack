64e9106f9e1176e0778ed2c7932447edebea2601
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-26 08:16:09 -0800

    Deploy

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(session({
 // TESTING
 app.get('/PiggyBack/listWorkers', function(request, response) {
 	onfleet.getSingleTeamByID('ylC5klVbtmEVrVlBfUYp9oeM').then(function(data) {
-		response.render('error', {pageTitle: 'Success', error: JSON.stringify(data)})
+		response.render('error', {pageTitle: 'Success', error: JSON.stringify(data.workers)})
 	}, function(error) {
 		response.render('error', {pageTitle: 'Error', error: JSON.stringify(error)})
 	})
