9762c4cebeaaefc78548c30791ca8cb891a91ae7
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-27 21:55:36 -0800

    Deploy

--- onfleet.js
+++ onfleet.js
@@ -145,10 +145,10 @@ function listDestinationsByID(arr) {
 				reject(error)
 			})
 		}
-		if (temp.length == arr.length)
+		//if (temp.length == arr.length)
 			resolve(temp)
-		else
-			reject('One or more ids did not work')
+		//else
+		//	reject('One or more ids did not work')
 	})
 }
 
