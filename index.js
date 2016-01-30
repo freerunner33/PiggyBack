8477f7aa5036efa0c48ec453c5a37b9860384ff6
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-30 09:42:17 -0800

    Deploy

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ app.post('/Piggyback/jobs', function(request, response) {
 	var waypoint2 = {address: b.dropoff_address, address2: b.dropoff_address2 , city: b.dropoff_city, state: b.dropoff_state, zip: b.dropoff_zip, name: b.dropoff_name, phone: b.dropoff_phone, email: b.dropoff_email, location: {latitude: b.dropoff_latitude, longitude: b.dropoff_longitude}, arrive_at: b.dropoff_arrive_at, special_instructions: b.dropoff_special_instructions}
 	var newjob = {pickup_waypoint: waypoint1, dropoff_waypoint: waypoint2, order_id: b.order_id, order_items: b.order_items, order_total: b.order_total, tip: b.tip, support_phone: b.support_phone, debug: b.debug}
 
-	var dest = {name: 'USD', number: '5998', street: 'Alcala Park', apartment: '', city: 'San Diego', state: 'California', postalCode: '92110', country: 'USA'}
+	var dest = {address: {name: 'USD', number: '5998', street: 'Alcala Park', apartment: '', city: 'San Diego', state: 'California', postalCode: '92110', country: 'USA'}}
 	
 	onfleet.createNewTask(
 		'~2FSQGbR0qSXi1v9kSQxtW4v',		// merchant
