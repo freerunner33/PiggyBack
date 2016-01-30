ad5dcdc425483853cf579087aeb998a172c16248
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-30 08:48:49 -0800

    Deploy

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ app.post('/Piggyback/jobs', function(request, response) {
 
 	var waypoint1 = {address: b.pickup_address, address2: b.pickup_address2 , city: b.pickup_city, state: b.pickup_state, zip: b.pickup_zip, name: b.pickup_name, phone: b.pickup_phone, email: b.pickup_email, location: {latitude: b.pickup_latitude, longitude: b.pickup_longitude}, arrive_at: b.pickup_arrive_at, special_instructions: b.pickup_special_instructions}
 	var waypoint2 = {address: b.dropoff_address, address2: b.dropoff_address2 , city: b.dropoff_city, state: b.dropoff_state, zip: b.dropoff_zip, name: b.dropoff_name, phone: b.dropoff_phone, email: b.dropoff_email, location: {latitude: b.dropoff_latitude, longitude: b.dropoff_longitude}, arrive_at: b.dropoff_arrive_at, special_instructions: b.dropoff_special_instructions}
-	var newjob = {pickup_waypoint: waypoint1, dropoff_waypoint: waypoint2, order_id: b.order_id, order_items, order_items: b.order_items, order_total: b.order_total, tip: b.tip, support_phone: b.support_phone, debug: b.debug}
+	var newjob = {pickup_waypoint: waypoint1, dropoff_waypoint: waypoint2, order_id: b.order_id, order_items: b.order_items, order_total: b.order_total, tip: b.tip, support_phone: b.support_phone, debug: b.debug}
 
 	job = JSON.stringify(newjob)
 
