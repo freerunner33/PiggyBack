72fa484535ddf56e6fe246b6ae374d7cebbf5e3e
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-30 10:38:23 -0800

    Deploy

--- index.js
+++ index.js
@@ -293,7 +293,8 @@ app.post('/Piggyback/jobs', function(request, response) {
 	var waypoint2 = {address: b.dropoff_address, address2: b.dropoff_address2 , city: b.dropoff_city, state: b.dropoff_state, zip: b.dropoff_zip, name: b.dropoff_name, phone: b.dropoff_phone, email: b.dropoff_email, location: {latitude: b.dropoff_latitude, longitude: b.dropoff_longitude}, arrive_at: b.dropoff_arrive_at, special_instructions: b.dropoff_special_instructions}
 	var newjob = {pickup_waypoint: waypoint1, dropoff_waypoint: waypoint2, order_id: b.order_id, order_items: b.order_items, order_total: b.order_total, tip: b.tip, support_phone: b.support_phone, debug: b.debug}
 
-	var destA = {address: {number: '5998', street: 'Alcala Park', city: 'San Diego', country: 'USA'}}
+	// var destA = {address: {number: '5998', street: 'Alcala Park', city: 'San Diego', state: 'CA', postalCode: '92110', country: 'USA'}}
+	var destA = {address: {}, location: ['-117.158910', '32.715825']}
 	var recipient = {name: 'Noah', phone: '9703084693', notes: 'Dashingly handsome', skipSMSNotifications: 'false', skipPhoneNumberValidation: 'false'}
 
 	onfleet.createNewTask(
