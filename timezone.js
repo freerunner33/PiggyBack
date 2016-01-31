553a037aa431aceaa9f49e69a2a41fb54ca4ba0c
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-30 16:11:44 -0800

    Deploy

--- timezone.js
+++ timezone.js
@@ -38,7 +38,7 @@ function request(endpoint, method, data) {
 }
 
 function getTimeZone() {
-	return request('', 'POST', {'location': '39.6034810,-119.6822510', 'timestamp': '1331766000', 'key': apiKey})
+	return request('', 'GET', {'location': '39.6034810,-119.6822510', 'timestamp': '1331766000', 'key': apiKey})
 }
 
 module.exports = {getTimeZone: getTimeZone}
