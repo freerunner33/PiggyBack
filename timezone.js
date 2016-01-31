571c9f5432119ef9c8e0db2ec5f41df7a44af5b6
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-30 22:25:29 -0800

    Deploy

--- timezone.js
+++ timezone.js
@@ -7,14 +7,11 @@ var https = require('https')
 var hostname = 'maps.googleapis.com'
 var path = '/maps/api/timezone/json'
 
-// https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyC1DXxAHKBW10zZ3W1bYJhJpPv9cgtlDoM
 function request(method, data) {
 	return new Promise(function(resolve, reject) {
 		var request = https.request({
 			hostname: hostname,
 			path: path + '?location=' + data.latitude + ',' + data.longitude + '&timestamp=' + data.timestamp + '&key=' + apiKey,
-
-			// + data.latitude + ',' + data.longitude + '&timestamp=' + data.timestamp + '&key=' + apiKey,
 			method: method
 		}, function(response) {
 			var str = ''
@@ -41,7 +38,7 @@ function request(method, data) {
 }
 
 function getTimeZone() {
-	return request('POST', {latitude: '39.6034810', longitude: '-119.6822510', timestamp: '1331766000', key: apiKey})
+	return request('GET', {latitude: '39.6034810', longitude: '-119.6822510', timestamp: '1331766000', key: apiKey})
 }
 
 module.exports = {getTimeZone: getTimeZone}
