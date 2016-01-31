a2f5982557e6ffd849311766614e9e86ed9655d7
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-01-30 22:17:42 -0800

    Deploy

--- timezone.js
+++ timezone.js
@@ -12,7 +12,7 @@ function request(method, data) {
 	return new Promise(function(resolve, reject) {
 		var request = https.request({
 			hostname: hostname,
-			path: path + '?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyC1DXxAHKBW10zZ3W1bYJhJpPv9cgtlDoM',
+			path: path + '?location=39.6034810,-119.6822510&timestamp=1331161200&key=' + apiKey,
 			// + data.latitude + ',' + data.longitude + '&timestamp=' + data.timestamp + '&key=' + apiKey,
 			method: method
 		}, function(response) {
