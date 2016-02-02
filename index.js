3716861ef313726280535a8dfa8aee139c66dbd8
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-02 09:50:08 -0800

    Deploy

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ app.post('/Piggyback/jobs', function(request, response) {
 										(dateC).toISOString(),											// completeBefore	- in UTC
 										worker.id,														// workerId
 										worker.name,													// workerName
-										'' + taskB.destination.address.number + taskB.destination.address.street + ', ' + taskA.destination.address.apartment + ', ' + taskA.destination.address.city, // + ', ' + taskA.destination.address.state + ' ' + task.destination.address.postalCode,
+										'' + taskB.destination.address.number + taskB.destination.address.street + ', ' + taskA.destination.address.apartment + ', ' + taskA.destination.address.city + ', ' + taskA.destination.address.state, // + ' ' + task.destination.address.postalCode,
 										null,															// completionTime
 										null															// didSucceed
 									], 
