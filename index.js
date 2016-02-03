691f18a4163b26ab6db5f6048b0937a10550263f
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-02 16:45:51 -0800

    Deploy

--- index.js
+++ index.js
@@ -373,6 +373,7 @@ app.post('/Piggyback/jobs', function(request, response) {
 					)
 				}, function(error) {
 					// do something with the task here... just simply add it to the queue
+					throw error
 					response.writeHead(400, { 'Content-Type': 'application/json'})
 					response.write(JSON.stringify(error))
 					response.end()
