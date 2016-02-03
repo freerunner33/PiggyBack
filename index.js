7bc048caf70ca96104a6abd4ef012b69594e5458
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-03 08:12:54 -0800

    Deploy

--- index.js
+++ index.js
@@ -598,7 +598,7 @@ app.get('/Piggyback/webhook/taskStarted', function(request, response, next) {
 
 // Used to send a webhook request
 app.get('/Piggyback/sendwebhook', function(request, response) {
-	onfleet.createWebHook('107.170.198.205/Piggyback/webhook/taskStarted', 0).then(function(data) {
+	onfleet.createWebHook('http://107.170.198.205/Piggyback/webhook/taskStarted', 0).then(function(data) {
 		response.render('error', {pageTitle: 'Success', errors: [JSON.stringify(data)]})
 	}, function(error) {
 		response.render('error', {pageTitle: 'Error', errors: [JSON.stringify(error)]})
