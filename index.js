fba3f29769c6f264baba7db861e24c306cf3e2a8
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-03 00:45:29 -0800

    Deploy

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.get('/Piggyback/jobs/*', function(request, response) {
 		response.end()
 	} else {
 		response.writeHead(200, {'Content-Type': 'application/json'})
-		response.write(JSON.stringify(error))
+		response.write(JSON.stringify({error: 'error'}))
 		response.end()
 		// onfleet.getSingleTask(path[3]).then(function(task) {
 		// 	connection.query('SELECT yelpId,workerName FROM Tasks WHERE shortId=?', [task.shortId], function(error, rows) {
