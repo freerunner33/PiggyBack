ae6e35cd731ae518e9289a13becec862b7c031d3
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-03 08:18:25 -0800

    Deploy

--- index.js
+++ index.js
@@ -588,14 +588,14 @@ app.post('/Piggyback/webhook/taskUnassigned', function(request, response) {
 
 
 // Used to respond to webhook request
-app.get('/Piggyback/webhook/taskEta', function(request, response, next) {
+app.get('/Piggyback/webhook/taskArrival', function(request, response, next) {
 	response.send(request.originalUrl.split('=')[1])
 	return next()
 })
 
 // Used to send a webhook request
 app.get('/Piggyback/sendwebhook', function(request, response) {
-	onfleet.createWebHook('http://107.170.198.205/Piggyback/webhook/taskEta', 1).then(function(data) {
+	onfleet.createWebHook('http://107.170.198.205/Piggyback/webhook/taskArrival', 2).then(function(data) {
 		response.render('error', {pageTitle: 'Success', errors: [JSON.stringify(data)]})
 	}, function(error) {
 		response.render('error', {pageTitle: 'Error', errors: [JSON.stringify(error)]})
