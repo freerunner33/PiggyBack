310c2e68faec9cc91fc64209cab83bfc6dab1a41
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-05 17:41:53 -0800

    Deploy

--- index.js
+++ index.js
@@ -582,7 +582,7 @@ app.post('/Piggyback/webhook/taskUpdated', function(request, response) {
 app.post('/Piggyback/webhook/taskDeleted', function(request, response) {
 	console.log('\nWEBHOOK: taskDeleted\nID 8: Task deleted.')
 	console.log(JSON.stringify(request.body))
-	connection.query('UPDATE Tasks SET status = concat(ifnull(status,""), "a,b,c") where taskId="QWEGpvl1lG8ls3B5h*SDqnvc"', function(error, rows) {
+	connection.query('UPDATE Tasks SET status = concat(ifnull(status,""), "a,b,c") where taskId = ?', ['QWEGpvl1lG8ls3B5h*SDqnvc'], function(error, rows) {
 		if (error)
 			console.log('ERROR\n' + error)
 		else
