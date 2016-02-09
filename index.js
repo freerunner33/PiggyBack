76378cbbcd617972b8cfc329edce8210813efde0
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-09 12:18:52 -0800

    Deploy

--- index.js
+++ index.js
@@ -613,15 +613,16 @@ app.post('/Piggyback/webhook/workerDuty', function(request, response) {
 })
 app.post('/Piggyback/webhook/taskCreated', function(request, response) {
 	onfleet.getSingleTask(request.body.taskId).then(function(task) {
-		connection.query('INSERT INTO JobLogs (shortId, statusCode, status, timestamp) VALUES (?,?,?,?)', [task.shortId,'54','done_delivered',(new Date()).toISOString()], function(error, rows){
-			if (error)
-				console.log('ERROR - query')
-			if (rows) {
-				console.log('SUCCESS - query')
-				console.log(rows)
-			}
-			response.sendStatus(200)
-		})
+		if (!task.pickupTask) {		// Must be dropoff task
+			connection.query('INSERT INTO JobLogs (shortId, statusCode, status, timestamp) VALUES (?,?,?,?)', [task.shortId,'54','done_delivered',(new Date()).toISOString()], function(error, rows){
+				if (error)
+					console.log('ERROR - query')
+				if (rows) {
+					console.log('SUCCESS - query')
+				}
+				response.sendStatus(200)
+			})
+		}
 	}, function(error) {
 		console.log('TASK NOT FOUND')
 		response.sendStatus(200)
