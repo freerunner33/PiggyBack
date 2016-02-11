ea0c6375760cc74b0710eeee90a6ebd2682a5abd
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-11 00:35:04 -0800

    Deploy

--- index.js
+++ index.js
@@ -252,11 +252,11 @@ function writeLog(arr, latitude, longitude) {
 				var status = eat24StatusCodes[status_code]
 				var reason = eat24Reasons[status_code]
 				var time = log.timestamp // this is a number - convert to local with tz, then format with tz addition -0800
+				resolve(arr.length)
 				time = time + offset.number
 				time = (new Date(time)).toISOString()
 				time = time.substring(0, time.length - 5) // 12:30:05.000Z
 				time = time + offset.string
-				resolve(arr.length)
 				newArr.push({
 					status_code: status_code,
 					status: status,
