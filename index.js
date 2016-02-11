b898a5773f6f125880f1bedc6f2e8cd2d8f056fa
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-11 08:09:12 -0800

    Deploy

--- index.js
+++ index.js
@@ -799,9 +799,8 @@ app.post('/Piggyback/webhook/taskUnassigned', function(request, response) {
 
 function getJobData(id) {
 	return new Promise(function(resolve, reject) {
-		resolve('hi')
 		var request = httpClient.request({
-			hostname: 'http://noahthomas.us',
+			hostname: 'noahthomas.us',
 			path: '/Piggyback' + '/jobs/' + id,
 			method: 'GET',
 			auth: user1 + ':' + pass1
