a90096bbeb15eb9771a8cb8c64c621707bc8ce60
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-12 08:29:30 -0800

    Deploy

--- timezone.js
+++ timezone.js
@@ -38,8 +38,7 @@ function request(method, data) {
 }
 
 function getTimeZone(lat, lon) {
-	var ts = (new Date()).getTime()
-	console.log('Time stamp: ' + ts)
+	console.log('Time stamp: ' + Date.now())
 	return request('GET', {latitude: lat, longitude: lon, timestamp: '1455294037', key: apiKey})
 	// latitude: '39.6034810', longitude: '-119.6822510'
 }
