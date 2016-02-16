63e0cf6c0d970af3de424cf687481f425d368980
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-16 13:37:18 -0800

    Deploy

--- index.js
+++ index.js
@@ -691,7 +691,7 @@ app.post('/Piggyback', function(request, response) {
 
 	if (username.localeCompare(yelpUser) == 0 && password.localeCompare(yelpPass) == 0) {
 		response.writeHead(200, { 'Content-Type': 'application/json' })
-		response.write(JSON.stringify(request.body))
+		response.write(request.body.name)
 		response.end()
 	} else {
 		response.sendStatus(401)
