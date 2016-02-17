0d76e573f7259ccdb5661e71dbad3929fffb59c2
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-17 07:41:14 -0800

    Deploy

--- index.js
+++ index.js
@@ -722,7 +722,7 @@ app.post('/Piggyback/export', function(request, response) {
 		var timeA = new Date(request.body.start_time).getTime()
 		timeA = timeA - (timezone.rawOffset * 1000) - (timezone.dstOffset * 1000)
 		var date = new Date(timeA)
-		response.render('error', {pageTitle: 'Export', errors: [date, timezone.rawOffset * 1000, timezone.dstOffset * 1000]})
+		response.render('error', {pageTitle: 'Export', errors: [date.toISOString(), timezone.rawOffset * 1000, timezone.dstOffset * 1000]})
 	}, function(error) {
 		response.render('error', {pageTitle: 'Error', errors: [JSON.stringify(error)]})
 	})
