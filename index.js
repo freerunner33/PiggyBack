408d4badcce3ad21ca8ec45ee5004e75d74b1c36
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-19 07:56:59 -0800

    Deploy

--- index.js
+++ index.js
@@ -715,9 +715,13 @@ app.post('/Piggyback/export', function(request, response) {
 	if (request.session.loggedin) {
 		timezone.getTimeZone(32.715869, -117.158959).then(function(timezone) {
 			// request.body.start_time.replace(/\u2010/g, '-');
-			var timeA = new Date(request.body.start_time).getTime()
-			timeA = timeA - (timezone.rawOffset * 1000) - (timezone.dstOffset * 1000)
-			var date = new Date(timeA)
+			if (request.body.start_time) {
+				var timeA = new Date(request.body.start_time).getTime()
+				timeA = timeA - (timezone.rawOffset * 1000) - (timezone.dstOffset * 1000)
+				var date = new Date(timeA)
+			} else {
+				date='no date given'
+			}
 			var query = 'shortId, driverTip, taskType, completeAfter, completeBefore, workerId, workerName, destination, completionTime, didSucceed'
 			var order = request.body.sort
 			connection.query('SELECT ' + query + ' FROM Tasks ORDER BY ' + order, [], function(error, rows) {
