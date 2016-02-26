ffbd9fc393075f082d1f8057d819b46274f675b9
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-26 14:02:54 -0800

    Deploy

--- index.js
+++ index.js
@@ -886,8 +886,8 @@ app.get('/Piggyback/download', function(request, response) {
 	// query += "INTO OUTFILE '/tmp/Piggyback_log01.csv' FIELDS ENCLOSED BY '\"' TERMINATED BY ',' ESCAPED BY '\\' LINES TERMINATED BY '\n'"
 	// query = encodeURI(query)
 
-	var query = 'select "shortId","taskId","driverTip" UNION select shortId,taskId,driverTip from Tasks into outfile "/tmp/test.txt" fields enclosed by "\\"" terminated by "," lines terminated by "\\n"'
-	
+	var query = 'select "shortId","taskId","driverTip" UNION select shortId,taskId,driverTip from Tasks into outfile "/tmp/test.txt" fields enclosed by "\"" terminated by "," lines terminated by "\n"'
+
 	console.log('QUERY\n' + query)
 
 	connection.query(query, function(error, rows) {
