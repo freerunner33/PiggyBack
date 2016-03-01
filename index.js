856c29fd07d5723b01cb2a3bf7c9c5dd63d4bfd3
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-03-01 09:46:35 -0800

    Deploy

--- index.js
+++ index.js
@@ -887,7 +887,7 @@ app.post('/Piggyback/download', function(request, response) {
 		query = query + "WHERE completeAfter >= '" + request.body.start_time + "' && completeAfter <= '" + request.body.end_time + "' && company = '" + request.body.company + "' ORDER BY shortId ";
 		query = query + "INTO OUTFILE " + file + " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
 
-		var query = "(SELECT workerName, workerId,didSucceed from Tasks) UNION (SELECT shortId, taskId, company from Tasks "
+		var query = "(SELECT 'workerName', 'workerId', 'didSucceed') UNION (SELECT shortId, taskId, company from Tasks "
 		query += "INTO OUTFILE " + file + " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n')"
 
 		connection.query(query, function(error, rows) {
