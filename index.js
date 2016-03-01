7d8a453515441906ab435f19420880942f3256bd
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-29 16:53:36 -0800

    Deploy

--- index.js
+++ index.js
@@ -874,7 +874,7 @@ app.get('/Piggyback/download', function(request, response) {
 	fs.readdir('/tmp', function (err, files) {
 		if (err)
 			throw err
-		var num = 1
+		var max = 1
 		for (var index in files) {
 			if (files[index].includes('Piggyback_log')) {
 				var num = (parseInt(files[index].substr(13, (files[index].indexOf('.')) - 13)))
