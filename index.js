b633240187e31aea590a68d32494da3ccf36b225
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-03-01 10:04:22 -0800

    Deploy

--- index.js
+++ index.js
@@ -882,10 +882,10 @@ app.post('/Piggyback/download', function(request, response) {
 			}
 		}
 		var file = "'/tmp/Piggyback_log" + (max + 1) + ".csv'"
-		var query = "SELECT 'shortId','taskId','yelpId','company','driverTip','taskType','completeAfter','completeBefore','workerId','workerName','destination','completionTime','didSucceed' ";
-		query = query + "UNION ALL SELECT shortId,taskId,yelpId,company,driverTip,taskType,completeAfter,completeBefore,workerId,workerName,destination,completionTime,didSucceed FROM Tasks ";
+		var query = "(SELECT 'shortId','taskId','yelpId','company','driverTip','taskType','completeAfter','completeBefore','workerId','workerName','destination','completionTime','didSucceed') ";
+		query = query + "UNION ALL (SELECT shortId,taskId,yelpId,company,driverTip,taskType,completeAfter,completeBefore,workerId,workerName,destination,completionTime,didSucceed FROM Tasks ";
 		query = query + "WHERE completeAfter >= '" + request.body.start_time + "' && completeAfter <= '" + request.body.end_time + "' && company = '" + request.body.company + "' ORDER BY shortId ";
-		query = query + "INTO OUTFILE " + file + " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
+		query = query + "INTO OUTFILE " + file + " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n')"
 
 		// var query = "(SELECT 'workerName', 'workerId', 'didSucceed') UNION ALL (SELECT shortId, destination, company FROM Tasks ORDER BY destination "
 		// query += "INTO OUTFILE " + file + " FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n')"
