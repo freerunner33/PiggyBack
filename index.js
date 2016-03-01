93a7ae21a38919eba89b28d22e3f1b3f23647d8f
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-29 22:27:34 -0800

    Deploy

--- index.js
+++ index.js
@@ -741,7 +741,7 @@ app.post('/Piggyback/export', function(request, response) {
 		var dateStrB = (new Date(timeB)).toISOString()
 		
 		var query = 'shortId, driverTip, taskType, workerName, destination, completionTime, didSucceed'
-		connection.query('SELECT ' + query + ' FROM Tasks WHERE completeAfter >= ? && completeAfter <= ? && company = ? ORDER BY destination', [dateStrA, dateStrB, request.body.company, request.body.order], function(error, rows) {
+		connection.query('SELECT ' + query + ' FROM Tasks WHERE completeAfter >= ? && completeAfter <= ? && company = ? ORDER BY ' + request.body.order, [dateStrA, dateStrB, request.body.company, request.body.order], function(error, rows) {
 			if (error)
 				throw error
 			if (rows && rows.length) {
